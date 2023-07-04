7094ad7f6c65c14cc23781829b45ccf4713fc6f0
Brigitte <brigitteorina@gmail.com>
2023-07-04 12:57:10 +0300

    complete

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,8 +1,37 @@
 class ItemsController < ApplicationController
+before_action :set_user
 
+  # GET /users/:user_id/items
   def index
-    items = Item.all
-    render json: items, include: :user
+    @items = @user.items
+    render json: @items
   end
+
+  # GET /users/:user_id/items/:id
+  def show
+    @item = @user.items.find(params[:id])
+    render json: @item
+  end
+
+  # POST /users/:user_id/items
+  def create
+    @item = @user.items.build(item_params)
+    if @item.save
+      render json: @item, status: :created
+    else
+      render json: @item.errors, status: :unprocessable_entity
+    end
+  end
+
+  private
+
+  def set_user
+    @user = User.find(params[:user_id])
+  end
+
+  def item_params
+    params.require(:item).permit(:name, :description, :price)
+  end
+  
 
 end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,11 @@
 class UsersController < ApplicationController
 
+  def index
+    user = User.find(params[:user_id])
+    items = user.items
+    render json: items
+  end
+
   def show
     user = User.find_by(id: params[:id])
     render json: user, include: :items
